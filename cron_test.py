43612b0a72fd4bbbe9a79682d87f8ef6ee138902
e-kaneko <ejieijih@gmail.com>
2025-02-13 19:37:41 +0900

    japanese time, and keep running

--- cron_test.py
+++ cron_test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo  # Python 3.9以降で利用可能
 
 def main():
-    print("Cron Test: This script ran at", datetime.datetime.now().isoformat())
+    now_utc = datetime.now(timezone.utc)
+    # 'Asia/Tokyo' は日本標準時（JST、UTC+9）を表します
+    now_jst = now_utc.astimezone(ZoneInfo("Asia/Tokyo"))
+    print("Cron Test: This script ran at", now_jst.isoformat())
 
 if __name__ == '__main__':
     main()
